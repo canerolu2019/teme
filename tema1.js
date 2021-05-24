a6c766ebbf1f144780ed7f6f53ece242626ae877
canerolu2019 <canerolu1@gmail.com>
2021-05-24 13:04:13 +0300

    teme

---
+++ tema1.js
@@ -0,0 +1,11 @@
+// Function that takes 2 paramiters (values)
+function isEqual(first, second) {
+    // If block compares if the values are equal
+    if (first === second) {
+      // If the condition from if checking is true then return true
+      return true
+    } else { 
+      // If the condition from if is false the return false
+      return false;
+    }
+  }
